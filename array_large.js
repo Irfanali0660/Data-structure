2c1403ca57ee44600cbb6bcbadb272bf2a9a93b0
irfanali <irfu3844@gmail.com>
2023-05-28 18:06:31 +0530

    s

--- array_large.js
+++ array_large.js
@@ -52,3 +52,76 @@
 
 // console.log(insert([3,4,5,455,3,10],111,0));
 
+class Node{
+    constructor(value){
+        this.value=value
+        this.next=null
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head=null
+        this.tail=null
+        this.length=0
+    }
+
+    prepend(value){
+        const node=new Node(value)
+
+        if(this.length==0){
+            this.head=node
+            this.tail=node
+        }else{
+            node.next=this.head
+            this.head=node
+            if (!node.next) {
+                this.tail = node;
+              }
+        }
+    this.length++
+    }
+
+    append(value){
+        const node=new Node(value)
+
+        if(this.length==0){
+            this.head=node
+            this.tail=node
+        }else{
+            this.tail.next=node
+           this.tail=node
+        }
+        this.length++
+    }
+
+    print(){
+        if(this.length==0){
+            return console.log('empty');
+        }else{
+            let curr=this.head
+            let values=''
+            while (curr) {
+                values+=`${curr.value} `
+                curr=curr.next
+            }
+            console.log(values);
+        }
+    }
+    header(){
+        console.log(this.head);
+        console.log(this.tail);
+    }
+    
+    
+}
+
+const list=new LinkedList()
+
+list.prepend(10)
+list.prepend(20)
+list.prepend(30)
+list.append(40)
+
+list.print()
+list.header()
--- back/tail.js
+++ back/tail.js
@@ -9,6 +9,7 @@ class Node {
     constructor() {
       this.head = null;
       this.tail = null;
+      this.size=0;
     }
   
     addToEnd(value) {
@@ -19,9 +20,27 @@ class Node {
         this.tail = newNode;
         return;
       }
-  
+      
+      console.log(this.tail,'before');
       this.tail.next = newNode;
       this.tail = newNode;
+      console.log(this.tail,'after');
+      this.size++
+    }
+
+    print(){
+        if(this.size==0){
+            return console.log("Empty");
+        }else{
+            let curr=this.head
+            let listvalues=''
+
+            while (curr) {
+                listvalues+=`${curr.value} `
+                curr=curr.next
+            }
+            console.log(listvalues);
+        }
     }
   }
   
@@ -30,6 +49,7 @@ class Node {
 linkedList.addToEnd(1);
 linkedList.addToEnd(2);
 linkedList.addToEnd(3);
+linkedList.print()
 
 console.log(linkedList.head); // Output: Node { value: 1, next: Node { value: 2, next: Node { value: 3, next: null } } }
 console.log(linkedList.tail); // Output: Node { value: 3, next: null }
